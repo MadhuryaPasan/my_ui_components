605ac827280acc365bff201a5fbb99d6285f9589
p.k.m.p.perera <p.k.m.p.perera@gmail.com>
2025-06-18 19:50:42 +0530

    Update page.tsx

--- src/app/components/page.tsx
+++ src/app/components/page.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import Ui_components_template from "@/components/Ui_components_template";
-import {Btn_list} from "@/components/components_library/Buttons/Btn_list";
+import {Btn_list,BtnType} from "@/components/components_library/Buttons/Btn_list";
+
 
 const components = () => {
   return (
     <>
       <div>components</div>
 
-      {Btn_list.map((data: any) => (
+      {Btn_list.map((data: BtnType) => (
         <Ui_components_template key={data.id} {...data} />
       ))}
     </>
